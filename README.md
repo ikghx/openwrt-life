71dff2bf3ca1c5c16515482fd5ed2b700f58a3ae
ikghx <admin@ikghx.com>
2022-02-18 12:02:55 +0800

    clean

---
+++ README.md
@@ -0,0 +1,134 @@
+![OpenWrt logo](include/logo.png)
+
+OpenWrt Project is a Linux operating system targeting embedded devices. Instead
+of trying to create a single, static firmware, OpenWrt provides a fully
+writable filesystem with package management. This frees you from the
+application selection and configuration provided by the vendor and allows you
+to customize the device through the use of packages to suit any application.
+For developers, OpenWrt is the framework to build an application without having
+to build a complete firmware around it; for users this means the ability for
+full customization, to use the device in ways never envisioned.
+
+Sunshine!
+
+ -------------------------------------------------------------------------------
+
+警告！
+本仓库仅供测试，不提供可靠性保证！
+
+Warning!
+This warehouse is for testing only and does not provide reliability guarantees!
+
+ -------------------------------------------------------------------------------
+ 
+## 编译准备
+
+1. 选择 “最小安装” Ubuntu 20.04.3 Desktop (64-bit)
+
+2. 使用普通用户登录 Ubuntu 系统，禁止使用 root 用户或权限进行编译操作。
+
+3. 安装编译环境
+
+```
+sudo apt-get update
+```
+
+```
+sudo apt install -y build-essential ccache ecj fastjar gawk \
+gettext git java-propose-classpath libelf-dev libncurses5-dev \
+libncursesw5-dev libssl-dev python-is-python2 python2.7-dev \
+python3-distutils python3-setuptools subversion swig xsltproc \
+upx-ucl libtinfo5 g++-multilib qemu-utils curl
+```
+
+```
+sudo apt-get clean
+```
+
+### 开始编译
+
+4. 获取主源代码
+
+```
+git clone -b openwrt-21.02 https://github.com/ikghx/openwrt-life.git
+```
+
+5. 进入主源代码目录，更新并安装软件库
+
+```
+./scripts/feeds update -a
+```
+
+```
+./scripts/feeds install -a
+```
+
+6. 打开编译菜单界面，按个人需要进行定制
+
+```
+make menuconfig
+```
+
+7. 开始编译
+
+```
+make
+```
+
+### 其它参考命令
+
+多线程加速编译，例如系统配备了4核心处理器
+
+```
+make -j4
+```
+
+编译时显示详细信息，用于排查编译错误
+
+```
+make V=s
+```
+
+打开内核菜单界面，按需定制内核功能
+
+```
+make kernel_menuconfig
+```
+
+清除软件编译缓存目录，以便快速测试软件更改
+
+```
+make clean
+```
+
+清空整个编译缓存目录，以便开始全新编译
+
+```
+make dirclean
+```
+
+## Support Information
+
+For a list of supported devices see the [OpenWrt Hardware Database](https://openwrt.org/supported_devices)
+
+### Documentation
+
+* [Quick Start Guide](https://openwrt.org/docs/guide-quick-start/start)
+* [User Guide](https://openwrt.org/docs/guide-user/start)
+* [Developer Documentation](https://openwrt.org/docs/guide-developer/start)
+* [Technical Reference](https://openwrt.org/docs/techref/start)
+
+### Support Community
+
+* [Forum](https://forum.openwrt.org): For usage, projects, discussions and hardware advise.
+* [Support Chat](https://webchat.oftc.net/#openwrt): Channel `#openwrt` on **oftc.net**.
+
+### Developer Community
+
+* [Bug Reports](https://bugs.openwrt.org): Report bugs in OpenWrt
+* [Dev Mailing List](https://lists.openwrt.org/mailman/listinfo/openwrt-devel): Send patches
+* [Dev Chat](https://webchat.oftc.net/#openwrt-devel): Channel `#openwrt-devel` on **oftc.net**.
+
+## License
+
+OpenWrt is licensed under GPL-2.0
---
+++ package/kernel/linux/modules/virt.mk
@@ -0,0 +1,134 @@
+#
+# Copyright (C) 2016 Yousong Zhou <yszhou4tech@gmail.com>
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+define KernelPackage/irqbypass
+  SUBMENU:=Virtualization
+  TITLE:=IRQ offload/bypass manager
+  KCONFIG:=CONFIG_IRQ_BYPASS_MANAGER
+  HIDDEN:=1
+  FILES:= $(LINUX_DIR)/virt/lib/irqbypass.ko
+  AUTOLOAD:=$(call AutoProbe,irqbypass.ko)
+endef
+$(eval $(call KernelPackage,irqbypass))
+
+
+define KernelPackage/kvm-x86
+  SUBMENU:=Virtualization
+  TITLE:=Kernel-based Virtual Machine (KVM) support
+  DEPENDS:=@TARGET_x86_generic||TARGET_x86_64 +kmod-irqbypass
+  KCONFIG:=\
+	  CONFIG_KVM \
+	  CONFIG_VIRTUALIZATION=y
+  FILES:= $(LINUX_DIR)/arch/$(LINUX_KARCH)/kvm/kvm.ko
+  AUTOLOAD:=$(call AutoProbe,kvm.ko)
+endef
+
+define KernelPackage/kvm-x86/description
+  Support hosting fully virtualized guest machines using hardware
+  virtualization extensions.  You will need a fairly recent
+  processor equipped with virtualization extensions. You will also
+  need to select one or more of the processor modules.
+
+  This module provides access to the hardware capabilities through
+  a character device node named /dev/kvm.
+endef
+
+$(eval $(call KernelPackage,kvm-x86))
+
+
+define KernelPackage/kvm-intel
+  SUBMENU:=Virtualization
+  TITLE:=KVM for Intel processors support
+  DEPENDS:=+kmod-kvm-x86
+  KCONFIG:=CONFIG_KVM_INTEL
+  FILES:= $(LINUX_DIR)/arch/$(LINUX_KARCH)/kvm/kvm-intel.ko
+  AUTOLOAD:=$(call AutoProbe,kvm-intel.ko)
+endef
+
+define KernelPackage/kvm-intel/description
+  Provides support for KVM on Intel processors equipped with the VT
+  extensions.
+endef
+
+$(eval $(call KernelPackage,kvm-intel))
+
+
+define KernelPackage/kvm-amd
+  SUBMENU:=Virtualization
+  TITLE:=KVM for AMD processors support
+  DEPENDS:=+kmod-kvm-x86
+  KCONFIG:=CONFIG_KVM_AMD
+  FILES:= $(LINUX_DIR)/arch/$(LINUX_KARCH)/kvm/kvm-amd.ko
+  AUTOLOAD:=$(call AutoProbe,kvm-amd.ko)
+endef
+
+define KernelPackage/kvm-amd/description
+  Provides support for KVM on AMD processors equipped with the AMD-V
+  (SVM) extensions.
+endef
+
+$(eval $(call KernelPackage,kvm-amd))
+
+define KernelPackage/vfio-mdev
+  SUBMENU:=Virtualization
+  TITLE:=VFIO driver support to to virtualize devices
+  DEPENDS:=@TARGET_x86_64
+  KCONFIG:=	\
+	  CONFIG_IOMMU_API=y	\
+	  CONFIG_MMU=y	\
+	  CONFIG_VFIO=y	\
+	  CONFIG_VFIO_NOIOMMU=y	\
+	  CONFIG_VFIO_PCI=y	\
+	  CONFIG_VFIO_PCI_IGD=y	\
+	  CONFIG_VFIO_MDEV	\
+	  CONFIG_VFIO_MDEV_DEVICE
+  FILES:=	\
+	  $(LINUX_DIR)/drivers/vfio/mdev/mdev.ko	\
+          $(LINUX_DIR)/drivers/vfio/mdev/vfio_mdev.ko
+  AUTOLOAD:=$(call AutoProbe,mdev vfio_mdev)
+endef
+
+define KernelPackage/vfio-mdev/description
+  Provides a framework to virtualize devices.
+endef
+
+$(eval $(call KernelPackage,vfio-mdev))
+
+define KernelPackage/i915-gvt
+  SUBMENU:=Virtualization
+  TITLE:=Enable KVM/VFIO support for Intel GVT-g
+  DEPENDS:=@TARGET_x86_64 +kmod-kvm-intel +kmod-drm-i915 +kmod-vfio-mdev
+  KCONFIG:=    \
+	  CONFIG_DRM_I915_GVT_KVMGT=m
+  FILES:=$(LINUX_DIR)/drivers/gpu/drm/i915/gvt/kvmgt.ko
+  AUTOLOAD:=$(call AutoProbe,kvmgt)
+endef
+
+define KernelPackage/i915-gvt/description
+  Intel GVT-g is a technology that provides mediated device
+  passthrough for Intel GPUs (Broadwell and newer). It can
+  be used to virtualize the GPU for multiple guest virtual
+  machines, effectively providing near-native graphics performance
+  in the virtual machine and still letting your host use the
+  virtualized GPU normally.
+
+  Choose this option if you want to enable Intel GVT-g graphics
+  virtualization technology host support with integrated graphics.
+  With GVT-g, it's possible to have one integrated graphics
+  device shared by multiple VMs under different hypervisors.
+
+  Note that at least one hypervisor like Xen or KVM is required for
+  this driver to work, and it only supports newer device from
+  Broadwell+. For further information and setup guide, you can
+  visit: http://01.org/igvt-g.
+
+  Now it's just a stub to support the modifications of i915 for
+  GVT device model. It requires at least one MPT modules for Xen/KVM
+  and other components of GVT device model to work. Use it under
+  you own risk.
+endef
+
+$(eval $(call KernelPackage,i915-gvt))
