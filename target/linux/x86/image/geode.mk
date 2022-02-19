243716904de3f465402905b78e1874eb00b31bbe
ikghx <admin@ikghx.com>
2022-02-19 20:50:07 +0800

    sync

--- target/linux/x86/image/geode.mk
+++ target/linux/x86/image/geode.mk
@@ -1,6 +1,7 @@
 define Device/generic
   DEVICE_TITLE := Generic x86/Geode
-  DEVICE_PACKAGES += kmod-crypto-cbc kmod-crypto-hw-geode kmod-ledtrig-gpio
+  DEVICE_PACKAGES += kmod-crypto-cbc kmod-crypto-ebc kmod-crypto-hw-geode \
+	kmod-ledtrig-gpio
   GRUB2_VARIANT := legacy
 endef
 TARGET_DEVICES += generic
