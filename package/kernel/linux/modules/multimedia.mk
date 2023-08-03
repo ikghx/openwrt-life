d6a2aaa140f022d61ccb8774b35b45d8ff7de10c
ikghx <admin@ikghx.com>
2023-08-03 09:30:37 +0800

    sync

--- package/kernel/linux/modules/multimedia.mk
+++ package/kernel/linux/modules/multimedia.mk
@@ -29,7 +29,8 @@ define KernelPackage/dvb-usb-v2
   DEPENDS:=+kmod-i2c-core +kmod-usb-core kmod-multimedia
   FILES:= \
 	$(LINUX_DIR)/drivers/media/dvb-core/dvb-core.ko \
-	$(LINUX_DIR)/drivers/media/usb/dvb-usb-v2/dvb_usb_v2.ko
+	$(LINUX_DIR)/drivers/media/usb/dvb-usb-v2/dvb_usb_v2.ko \
+	$(LINUX_DIR)/drivers/media/rc/rc-core.ko
   AUTOLOAD:=$(call AutoLoad,81,dvb-core dvb_usb_v2)
 endef
 define KernelPackage/dvb-usb-v2/description
