4fb28c1831b652536415b1fe21baf3fcbb1218db
ikghx <admin@ikghx.com>
2023-10-24 09:43:24 +0800

    sync

--- package/network/config/netifd/files/usr/libexec/network/packet-steering.sh
+++ package/network/config/netifd/files/usr/libexec/network/packet-steering.sh
@@ -6,31 +6,30 @@ NPROCS="$(grep -c "^processor.*:" /proc/cpuinfo)"
 PROC_MASK="$(( (1 << NPROCS) - 1 ))"
 
 find_irq_cpu() {
-        dev="$1"
-	    match="$(grep -m 1 "$dev\$" /proc/interrupts)"
-	    cpu=0
+    dev="$1"
+    match="$(grep -m 1 "$dev\$" /proc/interrupts)"
+    cpu=0
 
-	[ -n "$match" ] && {
-            set -- "$match"
-		shift
-		for cur in $(seq 1 $NPROCS); do
-			[ "$1" -gt 0 ] && {
-				cpu=$((cur - 1))
-				break
-			}
-			shift
-		done
-	}
-
-	echo "$cpu"
+    [ -n "$match" ] && {
+        set -- "$match"
+        shift
+        for cur in $(seq 1 "$NPROCS"); do
+            [ "$1" -gt 0 ] && {
+                cpu=$((cur - 1))
+                break
+            }
+            shift
+        done
+    }
+    echo "$cpu"
 }
 
 set_hex_val() {
-	file="$1"
-	val="$2"
-	val="$(printf %x "$val")"
+    file="$1"
+    val="$2"
+    val="$(printf %x "$val")"
     [ -n "$DEBUG" ] && echo "$file = $val"
-	echo "$val" > "$file"
+    echo "$val" > "$file"
 }
 
 packet_steering="$(uci get "network.@globals[0].packet_steering")"
@@ -41,35 +40,36 @@ flock 9 || exit 1
 
 [ -e "/usr/libexec/platform/packet-steering.sh" ] && {
     /usr/libexec/platform/packet-steering.sh
-	exit 0
+    exit 0
 }
 
 for dev in /sys/class/net/*; do
     [ -d "$dev" ] || continue
 
-    # ignore virtual interfaces
-    for file in "${dev}"/lower_*; do
-    if [ -e "$file" ]; then continue
+    # Ignore virtual interfaces
+    for file in "${dev}/lower_"*; do
+        if [ -e "$file" ]; then
+            continue
         fi
     done
-        
-	[ -d "${dev}/device" ] || continue
 
-	device="$(readlink "${dev}/device")"
-	device="$(basename "$device")"
-	irq_cpu="$(find_irq_cpu "$device")"
-	irq_cpu="$((1 << irq_cpu))"
+    [ -d "${dev}/device" ] || continue
 
-	for q in "${dev}"/queues/tx-*; do
-	    set_hex_val "$q/xps_cpus" "$PROC_MASK"
-	done
+    device="$(readlink "${dev}/device")"
+    device="$(basename "$device")"
+    irq_cpu="$(find_irq_cpu "$device")"
+    irq_cpu="$((1 << irq_cpu))"
 
-    # ignore dsa slave ports for RPS
-	subsys="$(readlink "${dev}/device/subsystem")"
-	subsys="$(basename "$subsys")"
-	[ "$subsys" = "mdio_bus" ] && continue
+    for q in "${dev}/queues/tx-"*; do
+        set_hex_val "$q/xps_cpus" "$PROC_MASK"
+    done
 
-	for q in "${dev}"/queues/rx-*; do
-	    set_hex_val "$q/rps_cpus" "$PROC_MASK"
-	done
+    # ignore DSA slave ports for RPS
+    subsys="$(readlink "${dev}/device/subsystem")"
+    subsys="$(basename "$subsys")"
+    [ "$subsys" = "mdio_bus" ] && continue
+
+    for q in "${dev}/queues/rx-"*; do
+        set_hex_val "$q/rps_cpus" "$PROC_MASK"
+    done
 done
