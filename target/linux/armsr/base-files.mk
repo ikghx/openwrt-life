26aad18dcc11d324b7eac2f941075430bb85dc61
ikghx <admin@ikghx.com>
2023-11-21 23:08:28 +0800

    sync

--- target/linux/armsr/base-files.mk
+++ target/linux/armsr/base-files.mk
@@ -3,8 +3,6 @@ ifeq ($(GRUB_SERIAL),)
 $(error This platform requires CONFIG_GRUB_SERIAL be set!)
 endif
 
-ifneq ($(GRUB_SERIAL),)
-  define Package/base-files/install-target
+define Package/base-files/install-target
 	$(SED) "s#@GRUB_SERIAL@#$(GRUB_SERIAL)#" $(1)/etc/inittab
-  endef
-endif
+endef
