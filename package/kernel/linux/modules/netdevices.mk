67808b91865980a3e56e045148d6b10b5adc4378
ikghx <admin@ikghx.com>
2023-11-29 16:44:28 +0900

    sync

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -1607,7 +1607,7 @@ $(eval $(call KernelPackage,stmmac-core))
 define KernelPackage/pcs-xpcs
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Synopsis DesignWare PCS driver
-  DEPENDS:=@TARGET_x86_64||TARGET_armsr_armv8
+  DEPENDS:=@TARGET_x86_64||TARGET_armsr_armv8 +kmod-phylink
   KCONFIG:=CONFIG_PCS_XPCS
   FILES:=$(LINUX_DIR)/drivers/net/pcs/pcs_xpcs.ko
   AUTOLOAD:=$(call AutoLoad,20,pcs_xpcs)
