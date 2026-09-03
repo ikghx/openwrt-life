d65de5ff767ad30d343b1d7fde0625740a9ca1f7
ikghx <admin@ikghx.com>
2026-09-02 22:32:57 -0700

    sync

--- package/network/config/wifi-scripts/files-ucode/lib/netifd/wireless/mac80211.sh
+++ package/network/config/wifi-scripts/files-ucode/lib/netifd/wireless/mac80211.sh
@@ -99,8 +99,8 @@ function setup_phy(phy, config, data) {
 		config.txpower = 'auto';
 
 	log(`Configuring '${phy}' txantenna: ${config.txantenna}, rxantenna: ${config.rxantenna} distance: ${config.distance}`);
-	system(`iw phy ${phy} set antenna ${config.txantenna} ${config.rxantenna}`);
-	system(`iw phy ${phy} set distance ${config.distance}`);
+	system(`iw phy ${phy} set antenna ${config.txantenna} ${config.rxantenna} >/dev/null 2>&1`);
+	system(`iw phy ${phy} set distance ${config.distance} >/dev/null 2>&1`);
 	system(`iw phy ${phy} set txpower ${config.txpower}`);
 
 	if (config.frag)
